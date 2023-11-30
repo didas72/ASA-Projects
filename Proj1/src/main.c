002070591af1fe6728b5357732384b040c0cff30
didas72 <diogocruzdiniz@gmail.com>
2023-11-30 18:10:20 +0000

    Automated testing

--- Proj1/src/main.c
+++ Proj1/src/main.c
@@ -24,13 +24,11 @@ int main()
 	/*Matrix alloc*/
 	maxMatrix = (int*)calloc((srcW + 1) * (srcH + 1), sizeof(int));
 	/*Possible rects*/
-	int x, y; 
+	int x, y, w, h, p;
 	scanf("%d", &x);
-	int w, h, p;
 	while ((--x) >= 0)
 	{
 		scanf("%d %d %d", &w, &h, &p);
-
 		if (MATRIX(w, h) < p)
 		{
 			if (w <= srcW && h <= srcH) MATRIX(w, h) = p;
@@ -46,26 +44,18 @@ int main()
 	/*Debug matrix*/
 	#ifdef DEBUG
 	printMatrix();
+	printf("FINAL: ");
 	#endif
-	/*Print result*/
+	/*Print result and exit*/
 	printf("%d\n", MATRIX(srcW, srcH));
-	/*Exit cleanly*/
 	return 0;
 }
 
 int calculateMax(int posX, int posY)
 {
-	int bestScore = MATRIX(posX, posY);
-	int i = 0;
-
-	for (i = 0; i <= DIV_UP(posX, 2); i++)
-		bestScore = MAX(bestScore, MATRIX(i, posY) + MATRIX(posX - i, posY));
-
-	for (i = 0; i <= DIV_UP(posY, 2); i++)
-	{
-		bestScore = MAX(bestScore, MATRIX(posX, i) + MATRIX(posX, posY - i));
-	}
-
+	int i, bestScore = MATRIX(posX, posY);
+	for (i = 0; i <= DIV_UP(posX, 2); i++) bestScore = MAX(bestScore, MATRIX(i, posY) + MATRIX(posX - i, posY));
+	for (i = 0; i <= DIV_UP(posY, 2); i++) bestScore = MAX(bestScore, MATRIX(posX, i) + MATRIX(posX, posY - i));
 	return MATRIX(posX, posY) = bestScore;
 }
 
@@ -74,17 +64,18 @@ int calculateMax(int posX, int posY)
 #ifdef DEBUG
 void printMatrix()
 {
+	int x, y;
 	printf("\n===MATRIX DUMP===\n X\\Y");
 
-	for (int y = 0; y <= srcH; y++) printf(" | %4d", y);
+	for (y = 0; y <= srcH; y++) printf(" | %4d", y);
 	printf("\n----");
-	for (int y = 0; y <= srcH; y++) printf("-+-----");
+	for (y = 0; y <= srcH; y++) printf("-+-----");
 	printf("-\n");
 
-	for (int x = 0; x <= srcW; x++)
+	for (x = 0; x <= srcW; x++)
 	{
 		printf("%4d", x);
-		for (int y = 0; y <= srcH; y++) printf(" | %4d", MATRIX(x, y));
+		for (y = 0; y <= srcH; y++) printf(" | %4d", MATRIX(x, y));
 		printf("\n");
 	}
 }
