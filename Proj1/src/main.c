e5ac9d2fb9deef90bcd3e046dc7cda87962a171b
didas72 <diogocruzdiniz@gmail.com>
2023-11-30 17:33:13 +0000

    Probably done

---
+++ Proj1/src/main.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma GCC diagnostic ignored "-Wunused-result"
+
+#define MAX(a, b) ((a > b) ? a : b)
+#define DIV_UP(a, b) ((a-1)/b+1)
+#define MATRIX(x, y) maxMatrix[INDEX(x, y)]
+#define INDEX(x, y) (1 + (x) + (y) * (srcW + 1))
+
+int calculateMax(int x, int y);
+
+#ifdef DEBUG
+void printMatrix();
+#endif
+
+int srcW, srcH, *maxMatrix;
+
+int main()
+{
+	/*Final rect*/
+	scanf("%d %d", &srcW, &srcH);
+	/*Matrix alloc*/
+	maxMatrix = (int*)calloc((srcW + 1) * (srcH + 1), sizeof(int));
+	/*Possible rects*/
+	int x, y; 
+	scanf("%d", &x);
+	int w, h, p;
+	while ((--x) >= 0)
+	{
+		scanf("%d %d %d", &w, &h, &p);
+
+		if (MATRIX(w, h) < p)
+		{
+			if (w <= srcW && h <= srcH) MATRIX(w, h) = p;
+			if (h <= srcW && w <= srcH) MATRIX(h, w) = p;
+		}
+	}
+	/*Compute*/
+	for (y = 1; y <= srcH; y++)
+	{
+		for (x = 1; x <= srcW; x++)
+			calculateMax(x, y);
+	}
+	/*Debug matrix*/
+	#ifdef DEBUG
+	printMatrix();
+	#endif
+	/*Print result*/
+	printf("%d\n", MATRIX(srcW, srcH));
+	/*Exit cleanly*/
+	return 0;
+}
+
+int calculateMax(int posX, int posY)
+{
+	int bestScore = MATRIX(posX, posY);
+	int i = 0;
+
+	for (i = 0; i <= DIV_UP(posX, 2); i++)
+		bestScore = MAX(bestScore, MATRIX(i, posY) + MATRIX(posX - i, posY));
+
+	for (i = 0; i <= DIV_UP(posY, 2); i++)
+	{
+		bestScore = MAX(bestScore, MATRIX(posX, i) + MATRIX(posX, posY - i));
+	}
+
+	return MATRIX(posX, posY) = bestScore;
+}
+
+
+
+#ifdef DEBUG
+void printMatrix()
+{
+	printf("\n===MATRIX DUMP===\n X\\Y");
+
+	for (int y = 0; y <= srcH; y++) printf(" | %4d", y);
+	printf("\n----");
+	for (int y = 0; y <= srcH; y++) printf("-+-----");
+	printf("-\n");
+
+	for (int x = 0; x <= srcW; x++)
+	{
+		printf("%4d", x);
+		for (int y = 0; y <= srcH; y++) printf(" | %4d", MATRIX(x, y));
+		printf("\n");
+	}
+}
+#endif
