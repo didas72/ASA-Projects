3d9c9b5047c325a9ad65931fb19ab69d066c50c2
Didas <diogocruzdiniz@gmail.com>
2023-11-30 13:53:43 +0000

    Broken

--- Proj1/src/main.cpp
+++ Proj1/src/main.cpp
@@ -1,109 +1,217 @@
-//main.cpp - 
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Vec2
-{
-	int x;
-	int y;
-
-	Vec2() { x = 0; y = 0; }
-	Vec2(int vx, int vy) { x = vx; y = vy; }
-	Vec2 operator +(const Vec2& a) const
-	{ return Vec2(x + a.x, y + a.y); }
-} _Vec2;
+#define MAX(a, b) ((a > b) ? a : b)
 
 typedef struct Rect
 {
-	Vec2 corner;
+	int w, h;
 	int price;
+	size_t area;
 
-	//NOTE: Max cost capped at 2^20 ~= 1M (assuming 32-bit int) for fixed point density
-	int density;
-
-	Rect() : corner(Vec2(0,0)), price(0), density(0) {}
-	Rect(int rw, int rh, int rp) : corner(Vec2(rw, rh)), price(rp), density((rp << 12) / (rw * rh)) {}
+	Rect() : w(0), h(0), price(0), area(0) {}
+	Rect(int rw, int rh, int rp) : w(rw), h(rh), price(rp), area(rw*rh) {}
 } _Rect;
 
-void acceptSourceRect();
-void acceptPossibleRects();
-void calculateMaxValue();
-inline void insertRectSorted(int i, int w, int h, int p);
+typedef struct HalfMatrix
+{
+	int *store;
+	int w, h;
+	size_t tri;
+
+public:
+	HalfMatrix() : store(NULL), w(0), h(0) {}
+	HalfMatrix(int mw, int mh) : w(mw), h(mh) {
+		size_t triangle = (mw*(mw+1)) >> 1;
+		size_t rect = (mh-mw)*mw;
+		size_t total = triangle + rect;
+		tri = triangle;
+		store = (int*)malloc(total * sizeof(int));
+		memset(store, 0xFF, total * sizeof(int)); //set all to -1
+	}
+
+	inline int& operator() (int x, int y)
+	{
+		size_t index;
+		x -= 1;//start at index 0
+		y -= 1;//start at index 0
+
+		if (x > y)
+		{
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+
+		if (y < w) //triangle
+			index = (y*(y+1)>>1) + x;
+		else //rect
+			index = tri + (y-w) * w + x;
+
+		//printf("{[%d,%d]=%ld}", x, y, index);
+		return store[index];
+	}
+} _HalfMatrix;
+
+inline void acceptSourceRect();
+inline void acceptPossibleRects();
+inline int calculateMax(int x, int y);
+inline int fillMatrix();
+
+#ifdef DEBUG
+void printMatrix();
+#endif
 
 Rect src;
 int requestCount;
 Rect *requests;
-int totalValue;
+HalfMatrix maxMatrix;
 
 int main()
 {
 	acceptSourceRect();
 	acceptPossibleRects();
 
-	calculateMaxValue();
+	maxMatrix = HalfMatrix(src.w, src.h);
 
-	printf("%d", totalValue);
+	fillMatrix();
+
+	int max = calculateMax(src.w, src.h);
+
+	#ifdef DEBUG
+	printMatrix();
+	#endif
 
 	return 0;
 }
 
 
-void acceptSourceRect()
+inline void acceptSourceRect()
 {
 	int w, h;
 	scanf("%d %d", &w, &h);
-	src = Rect(w, h, 0);
+	if (w <= h) //Make rectangle vertical
+		src = Rect(w, h, 0);
+	else
+		src = Rect(h, w, 0);
 }
 
-void acceptPossibleRects()
+inline void acceptPossibleRects()
 {
 	scanf("%d", &requestCount);
 
+	requestCount <<= 1;
 	requests = (Rect*)malloc(requestCount * sizeof(Rect));
 
 	int i = 0; int w, h, p;
-	while (i <= requestCount)
+	while (i+1 < requestCount)
 	{
 		scanf("%d %d %d", &w, &h, &p);
+		requests[i] = Rect(w, h, p);
+		requests[i+1] = Rect(h, w, p);
 		i += 2;
-		insertRectSorted(i, w, h, p);
 	}
 }
 
-inline void insertRectSorted(int i, int w, int h, int p)
+inline int calculateMax(int x, int y)
 {
-	//TODO: Maybe input with dlinked list and then compact to array
+	int buff = maxMatrix(x, y);
+	if (buff != -1)
+		return buff;
 
-	Rect norm = Rect(h, w, p);
-	Rect rot = Rect(h, w, p);
-	int density = norm.density;
+	int bestScore = 0;
+	int bestIndex = -1;
 
-	int max = i >> 1, min = 0, c;
-
-	while (max - min > 1)
+	for (int i = 0; i < requestCount; i++)
 	{
-		c = (max + min) >> 1;
+		Rect me = requests[i];
 
-		if (density > requests[c << 1].density)
-			min = c;
-		else if (density <= requests[c << 1].density) //TODO: Prioritise smaller area
-			max = c;
+		//Check piece fits
+		if (me.w > x || me.h > y) continue;
+
+		int score = me.price;
+		int new1X, new1Y, new2X, new2Y;
+
+		//Cut this particular piece
+		if (me.w < x) //must cut vertically
+		{
+			new1X = x - me.w;
+			new1Y = y;
+
+			if (me.h < y) //must also cut horizontally (after vertical cut)
+			{
+				new2X = x;
+				new2Y = y - me.h;
+				score = MAX(score, me.price + calculateMax(new1X, new1Y) + calculateMax(new2X, new2Y));
+			}
+			else
+				score = MAX(score, me.price + calculateMax(new1X, new1Y));
+		}
+		if (me.h < y) //must cut horizontally
+		{
+			new1X = x;
+			new1Y = y - me.h;
+
+			if (me.w < x) //must also cut vertically (after horizontal cut)
+			{
+				new2X = x - me.w;
+				new2Y = y;
+				score = MAX(score, me.price + calculateMax(new1X, new1Y) + calculateMax(new2X, new2Y));
+			}
+			else
+				score = MAX(score, me.price + calculateMax(new1X, new1Y));
+		}
+
+		if (score > bestScore)
+		{
+			bestScore = score;
+			bestIndex = i;
+		}
 	}
 
-	int index = min << 1;
-
-	for (int j = i; j < index + 2; j -= 2)
-	{
-		requests[j] = requests[j-2];
-		requests[j+1] = requests[j-1];
-	}
-
-	requests[index] = norm;
-	requests[index+1] = rot;
+	return maxMatrix(x,y) = bestScore;
 }
 
-void calculateMaxValue()
+inline int fillMatrix()
 {
-	
+	for (int y = 1; y <= src.h; y++)
+	{
+		for (int x = 1; x <= y && x <= src.w; x++)
+		{
+			calculateMax(x, y);
+		}
+	}
+
+	return maxMatrix(src.w, src.h);
 }
+
+
+
+//Debugging
+#ifdef DEBUG
+void printMatrix()
+{
+	printf("\n===MATRIX DUMP===\n#   ");
+
+	for (int y = 1; y <= src.h; y++)
+		printf(" | %4d", y);
+	printf("\n----");
+	for (int y = 1; y <= src.h; y++)
+		printf("-+-----");
+	printf("-\n");
+
+	for (int x = 1; x <= src.w; x++)
+	{
+		printf("%4d", x);
+		for (int y = 1; y <= src.h; y++)
+		{
+			if (y >= x)
+				printf(" | %4d", maxMatrix(x, y));
+			else
+				printf(" |     ");
+		}
+		printf("\n");
+	}
+}
+#endif
