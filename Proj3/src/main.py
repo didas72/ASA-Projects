a3c007cdb226f1c92046acc83bdd9b8b3e36cca7
didas72 <diogocruzdiniz@gmail.com>
2023-12-29 11:47:30 +0000

    Fixed for right python version

--- Proj3/src/main.py
+++ Proj3/src/main.py
@@ -1,21 +1,21 @@
 from pulp import *
 parts = input().split()
 t, p, mpd = int(parts[0]), int(parts[1]), int(parts[2])
-vars : list[tuple[LpVariable, float, int, list[int]]] = []
-packs : list[tuple[LpVariable, float]] = []
+varl : 'list[tuple[LpVariable, float, int, list[int]]]' = []
+packs : 'list[tuple[LpVariable, float]]' = []
 prob = LpProblem(sense = const.LpMaximize)
-for iter in range(t):
+for iterc in range(t):
 	parts = input().split()
-	vars.append((LpVariable("Toy#"+str(iter+1), 0, cat = const.LpInteger), float(parts[0]), int(parts[1]), []))
-for iter in range(p):
+	varl.append((LpVariable("Toy#"+str(iterc+1), 0, cat = const.LpInteger), float(parts[0]), int(parts[1]), []))
+for iterc in range(p):
 	parts = input().split()
-	packs.append((LpVariable("Pack#"+str(iter+1), 0, cat = const.LpInteger), float(parts[3])))
-	vars[int(parts[0])-1][3].append(iter)
-	vars[int(parts[1])-1][3].append(iter)
-	vars[int(parts[2])-1][3].append(iter)
-prob += (lpSum([var[1] * var[0] for var in vars] + [pack[1] * pack[0] for pack in packs]), "Objective")
-for i in range(len(vars)):
-	prob += (lpSum([vars[i][0]] + [packs[varIdx][0] for varIdx in vars[i][3]]) <= vars[i][2], "Restriction#"+str(i+1))
-prob += (lpSum([var[0] for var in vars] + [3 * pack[0] for pack in packs]) <= mpd, "TotalRestriction")
-prob.solve(GLPK(msg=False))
-print(value(prob.objective))
+	packs.append((LpVariable("Pack#"+str(iterc+1), 0, cat = const.LpInteger), float(parts[3])))
+	varl[int(parts[0])-1][3].append(iterc)
+	varl[int(parts[1])-1][3].append(iterc)
+	varl[int(parts[2])-1][3].append(iterc)
+prob += (lpSum([var[1] * var[0] for var in varl] + [pack[1] * pack[0] for pack in packs]), "Objective")
+for i in range(len(varl)):
+	prob += (lpSum([varl[i][0]] + [packs[varIdx][0] for varIdx in varl[i][3]]) <= varl[i][2], "Restriction#"+str(i+1))
+prob += (lpSum([var[0] for var in varl] + [3 * pack[0] for pack in packs]) <= mpd, "TotalRestriction")
+prob.solve(GLPK(msg=0))
+print(int(value(prob.objective)))
